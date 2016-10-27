955fbc8512c011bdccedd02c51ed0c8836c61fda
mikrob <mikael.robert@botsunit.com>
2016-10-27 19:06:01 +0200

    comment main base call to allow easy testing of fleet integration

--- wowza.go
+++ wowza.go
@@ -2,12 +2,7 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"os"
-	"wowza-rolling-update/digest"
-	lib "wowza-rolling-update/lib"
-
-	"github.com/hashicorp/consul/api"
+	"wowza-rolling-update/lib"
 )
 
 var (
@@ -20,61 +15,63 @@ var (
 )
 
 func main() {
-	transport := digest.NewTransport("admin", "admin.123")
-	flag.Parse()
-	var tag lib.Tag
-	if *tagOpts != "" {
-		tag.DeconstructTag(*tagOpts)
-	}
+	// transport := digest.NewTransport("admin", "admin.123")
+	// flag.Parse()
+	// var tag lib.Tag
+	// if *tagOpts != "" {
+	// 	tag.DeconstructTag(*tagOpts)
+	// }
+	//
+	// client, err := api.NewClient(api.DefaultConfig())
+	// if err != nil {
+	// 	panic(err)
+	// }
+	//
+	// queryOpts := &api.QueryOptions{
+	// 	Datacenter: *datacenterName,
+	// }
+	//
+	// catalogServices, _, err := client.Catalog().Service(*serviceName, "", queryOpts)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	os.Exit(1)
+	// }
+	//
+	// if *listActionOpts {
+	// 	for _, s := range catalogServices {
+	// 		cs := lib.CatalogService{Dc: *datacenterName, Cs: s}
+	// 		if *tagOpts != "" && !cs.HasTag(tag) {
+	// 			continue
+	// 		}
+	// 		currentConnections, _ := lib.GetMetrics(cs.GetURL(), transport)
+	// 		fmt.Printf("[%s] node:%s lan:%s wan:%s tags:%s current_connections:%d\n",
+	// 			s.ServiceName,
+	// 			s.Node,
+	// 			s.TaggedAddresses["lan"],
+	// 			s.TaggedAddresses["wan"],
+	// 			s.ServiceTags,
+	// 			currentConnections.CurrentConnections,
+	// 		)
+	// 	}
+	// 	os.Exit(0)
+	// } else if *addTagActionOpts {
+	// 	service, err := lib.SearchServiceWithoutTag(catalogServices, tag)
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 		os.Exit(0)
+	// 	}
+	// 	cs := lib.CatalogService{Dc: *datacenterName, Cs: &service}
+	// 	err = cs.ServiceAddTag(client, &service, tag)
+	// 	if err != nil {
+	// 	}
+	// } else if *deleteTagActionOpts {
+	// 	for _, service := range catalogServices {
+	// 		cs := lib.CatalogService{Dc: *datacenterName, Cs: service}
+	// 		cs.ServiceDeleteTag(client, service, tag)
+	// 	}
+	// } else {
+	// 	flag.Usage()
+	// }
 
-	client, err := api.NewClient(api.DefaultConfig())
-	if err != nil {
-		panic(err)
-	}
-
-	queryOpts := &api.QueryOptions{
-		Datacenter: *datacenterName,
-	}
-
-	catalogServices, _, err := client.Catalog().Service(*serviceName, "", queryOpts)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	if *listActionOpts {
-		for _, s := range catalogServices {
-			cs := lib.CatalogService{Dc: *datacenterName, Cs: s}
-			if *tagOpts != "" && !cs.HasTag(tag) {
-				continue
-			}
-			currentConnections, _ := lib.GetMetrics(cs.GetURL(), transport)
-			fmt.Printf("[%s] node:%s lan:%s wan:%s tags:%s current_connections:%d\n",
-				s.ServiceName,
-				s.Node,
-				s.TaggedAddresses["lan"],
-				s.TaggedAddresses["wan"],
-				s.ServiceTags,
-				currentConnections.CurrentConnections,
-			)
-		}
-		os.Exit(0)
-	} else if *addTagActionOpts {
-		service, err := lib.SearchServiceWithoutTag(catalogServices, tag)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(0)
-		}
-		cs := lib.CatalogService{Dc: *datacenterName, Cs: &service}
-		err = cs.ServiceAddTag(client, &service, tag)
-		if err != nil {
-		}
-	} else if *deleteTagActionOpts {
-		for _, service := range catalogServices {
-			cs := lib.CatalogService{Dc: *datacenterName, Cs: service}
-			cs.ServiceDeleteTag(client, service, tag)
-		}
-	} else {
-		flag.Usage()
-	}
+	lib.ListFleetMachines()
 }
