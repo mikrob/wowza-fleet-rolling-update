2e0208f5773f233c6d5e365c458cdabb9306e4b6
Benjamin Jorand <benjamin.jorand@botsunit.com>
2016-11-07 17:21:44 +0100

    wip

--- wowza.go
+++ wowza.go
@@ -3,8 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
+	"time"
+	"wowza-rolling-update/digest"
 	"wowza-rolling-update/lib"
+
+	"github.com/hashicorp/consul/api"
 )
 
 var (
@@ -15,11 +20,72 @@ var (
 	deleteTagActionOpts = flag.Bool("delete-tag", false, "Delete tag")
 	listActionOpts      = flag.Bool("list", false, "List services")
 	unit                = flag.String("unit", "", "Unit to start")
+	update              = flag.String("update", "", "Image to update to")
 )
 
 func main() {
-	// transport := digest.NewTransport("admin", "admin.123")
+	transport := digest.NewTransport("admin", "admin.123")
 	flag.Parse()
+
+	if *update != "" && *serviceName != "" && *datacenterName != "" {
+		client, err := api.NewClient(api.DefaultConfig())
+		if err != nil {
+			panic(err)
+		}
+
+		queryOpts := &api.QueryOptions{
+			Datacenter: *datacenterName,
+		}
+
+		catalogServices, _, err := client.Catalog().Service(*serviceName, "", queryOpts)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		// loop start
+		// search for a service without this image name and tag it for future update (primarily includes already tagged service)
+		// wait until this service has no connections
+		// search unit linked to that service
+		// destroy unit
+		// start unit
+		// loop until all image tag are equals to update tag
+
+		updateTag := lib.Tag{Key: "update", Value: *update}
+		alreadyUpdatedTag := lib.Tag{Key: "image", Value: *update}
+
+		loopIndex := 1
+		for {
+			service, err := lib.SearchServiceWithoutTag(catalogServices, alreadyUpdatedTag)
+			if err != nil {
+				fmt.Println(err)
+			}
+			cs := lib.CatalogService{Dc: *datacenterName, Cs: &service}
+			err = cs.ServiceAddTag(client, &service, updateTag)
+			if err != nil {
+				fmt.Println(err)
+			}
+			currentConnections, _ := lib.GetMetrics(cs.GetURL(), transport)
+			if currentConnections.CurrentConnections == 0 {
+				log.Println(cs.Cs.ServiceName, cs.Cs.Address, cs.Cs.Node, currentConnections.CurrentConnections, "connections")
+				// search fleet machine
+				machines, _ := lib.ListFleetMachines()
+				for _, machine := range machines {
+					if machine.PublicIP == cs.Cs.Address {
+						lib.RunDestroyUnit(units, &cAPI)
+						lib.RunStartUnit()
+					}
+				}
+				// units, _ := lib.ListFleetUnits()
+				// for _, unit := range units {
+				// 	if unit.
+				// }
+			}
+			loopIndex += loopIndex
+			time.Sleep(1 * time.Second)
+		}
+
+	}
+	os.Exit(0)
 	// var tag lib.Tag
 	// if *tagOpts != "" {
 	// 	tag.DeconstructTag(*tagOpts)
